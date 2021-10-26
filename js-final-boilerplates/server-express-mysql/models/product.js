7d4f1d497ae9b6c94cc290d14031334909b68510
Nate Lamborn <NateJLamborn@gmail.com>
2021-10-25 23:25:13 -0400

    added product post

--- js-final-boilerplates/server-express-mysql/models/product.js
+++ js-final-boilerplates/server-express-mysql/models/product.js
@@ -24,6 +24,10 @@ module.exports = (sequelize, DataTypes) => {
         description: {
             type: DataTypes.STRING,
             allowNull: false
+        },
+        quantity: {
+            type: DataTypes.INTEGER,
+            allowNull: false
         }
     },
     {}
--- js-final-boilerplates/server-express-mysql/routes/inventory.js
+++ js-final-boilerplates/server-express-mysql/routes/inventory.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var models = require('../models');
+const users = require('../models/users');
 
 router.get('/blackShirt', function (req, res, next){
     models.stock.findOne({
@@ -27,24 +28,25 @@ router.get('/whiteShirt', function (req, res, next){
 })
 
 router.post('/addProduct', function (req, res, next){
-  models.product
-    .findOrCreate({
-      where: {
-        title: req.body.title
-      },
-      defaults: {
-        price: req.body.price,
-        imageUrl: req.body.imageUrl,
-        description: req.body.description
-      }
-    })
-    .spread(function (result, created) {
-      if (created) {
-        res.send('User successfully created');
-      } else {
-        res.send('This user already exists');
-      }
-    });
+    models.product
+      .findOrCreate({
+        where: {
+          title: req.body.title
+        },
+        defaults: {
+          price: req.body.price,
+          imageUrl: req.body.imageUrl,
+          description: req.body.description,
+          quantity: req.body.quantity
+        }
+      })
+      .spread(function (result, created) {
+        if (created) {
+          res.send('Product created successfully');
+        } else {
+          res.send('This product already exists');
+        }
+      });
 })
 
 module.exports = router;
